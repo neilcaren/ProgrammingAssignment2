eca80be58edbf7b0759d77326f227203d3e74eae
Neil Caren <neil.caren@gmail.com>
2015-10-25 15:00:28 -0400

    added comments to code ls
    ;s

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions work together to create and cache the inverse of a matrix.  
+## THis is handy when you have a large matrix and need its inveresre repeatlly.  
+## Instead of calculating the inverse each time its needed, these functiopns together manage 
+## a cached version located in the global environment and quickly return a cached version.
 
-## Write a short comment describing this function
+
+## This function essentially returns a list of functions designed to work with the cacheSolv function  
+## the function creates get, set, getinverse and setinverse functions to be used by cachedSolve 
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -18,8 +22,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-## Return a matrix that is the inverse of 'x'
+
+## THis function returns a matrix that is the inverse of 'x'
+## 'x' needs to be the object returned by makeCacheMatrix function
+## using the function hooks that are part of 'x', the function determines if the 
+## inverse needs to be calculated or simply returned from cache
+
 cacheSolve <- function(x, ...) {
         m <- x$getinverse()
         if(!is.null(m)) {
